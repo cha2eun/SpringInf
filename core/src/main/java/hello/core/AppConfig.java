7da28c5ec81de71aa8e9297d3b08c1d628346473
cha2eun <jan9chae2un@gmail.com>
2023-09-04 00:09:30 +0900

    '스프링빈'

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,14 +9,20 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 // 의존관계 주입
+@Configuration
 public class AppConfig {
+    // Bean 어노테이션 등록하면 메소드명이 key, return값이 value로 bean등록됨
     // 중복제거하고 한곳만 바꿀 수 있도록 리팩터링
     // 추후 구현체가 바뀌어도 AppConfig의 여기만 바꾸면 됨
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         // 할인정책 변경적용
@@ -24,10 +30,11 @@ public class AppConfig {
     }
 
     // 생성자 주입
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
---
+++ core/src/test/java/hello/core/beanfing/ApplicationContextInfoTest.java
@@ -0,0 +1,39 @@
+package hello.core.beanfing;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력")
+    void finaAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName+" + beanDefinitionName+" object=" +bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력")
+    void finaApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // ROLE_APPLICATION : 직접 등록한 애플리케이션빈
+            // ROLE_INFRASTRUCTURE : 스프링 내부 사용 빈
+//            if(beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE){
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName+" object=" +bean);
+            }
+        }
+    }
+
+}
